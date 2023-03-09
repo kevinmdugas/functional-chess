e44ec13b031a198f4bbe2184b5a250bea77c27fe
dev <kdugas@pdx.edu>
2023-03-09 03:47:08 -0800

    Setup test env

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+module Spec where
+
+import State
+import TestData
+
+import Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  _ <- runTestTT $ TestList [ testMove ]
+  return ()
+  
+-- The testMove data uses a board of dimensions 5 x 2 to be able to statically
+-- define multiple instances of expected board outputs with less repetitive code.
+testMove :: Test
+testMove = "testMove" ~:
+  TestList [
+    apply move ((0,0), (0,0)) initTestBoard ~?= (Nothing, initTestBoard),
+    apply move ((8,0), (0,0)) initTestBoard ~?= (Nothing, initTestBoard) 
+  ]
