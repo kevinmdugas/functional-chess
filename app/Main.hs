dd3290a9c12215ece4d0638253a79012bcf79c4b
kevinmdugas <77704104+kevinmdugas@users.noreply.github.com>
2023-03-09 03:43:56 -0800

    Add persistent game state (#4)
    * Created state object
    
    * Add module comments to state

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,37 @@
 module Main (main) where
+
+import qualified Text.Read as Text
   
 import Board
+import Piece
+import State
 import Interface
 
 main :: IO ()
 main = do
   mapM_ print menu
   choice <- getLine
-  if choice == "1" then play
+  if choice == "1" then play (Nothing, startState)
   else if choice == "2" then review
+  else if choice == "3" then return ()
   else do
     putStrLn "Invalid input"
     main
 
-play :: IO ()
-play = do
-  printBoard $ updateBoard emptyBoard startState
-  attemptedMove <- getLine
-  putStrLn attemptedMove
+play :: (Maybe Piece, Board) -> IO ()
+play (p, board) = do
+  printBoard board
+  putStrLn "curr: "
+  cur <- getLine
+  putStrLn "dest: "
+  dest <- getLine
+  play $ apply move (getPositions cur dest) board
+
+getPositions :: String -> String -> (Pos, Pos)
+getPositions cur dest = (Text.read cur :: Pos, Text.read dest :: Pos)
+
+endMatch :: String -> Bool
+endMatch s = s == "quit"
 
 review :: IO ()
 review = undefined
---
+++ src/State.hs
@@ -0,0 +1,43 @@
+module State where
+
+import Board
+import Piece
+
+{--
+  State object is only used after the move denoted by (Pos, Pos) has been
+  proven to be valid. It returns the updated board state after the move
+  and the captured piece if there is one.
+--}
+newtype ST = S { apply :: (Pos, Pos) -> Board -> (Maybe Piece, Board) }
+
+-- Return piece on the square denoted by the Pos arg
+getPiece :: Board -> Pos -> Maybe Piece
+getPiece board (x, y) =  piece (board !! y !! x )
+
+-- Perform valid move, returning the updated board state and either
+-- a captured piece or nothing
+move :: ST
+move = S $ \((a,b), (c,d)) board -> ( 
+    case ((a,b), (c,d)) of
+      ((i,j), (h,k)) | (i,j) == (h,k) -> (Nothing, board) 
+      ((i,j), (h,k)) |
+        (i >= 0 && i <= 7) && (j >= 0 && j <= 7) &&
+        (h >= 0 && h <= 7) && (k >= 0 && k <= 7) -> 
+          (
+            getPiece board (c,d),
+            updateBoard ((a,b), (c,d)) board
+          )
+      ((_, _), (_, _)) -> (Nothing, board)
+  )
+
+-- Update the board state by instantiating a duplicate of every square
+-- except the squares the player is moving from and to.
+updateBoard :: (Pos, Pos) -> Board -> Board
+updateBoard (cur, dest) board = [[
+  Square
+    (
+      if (i,j) == cur then Nothing
+      else if (i,j) == dest then getPiece board cur
+      else getPiece board (i,j)
+    )
+    (if even (i + j) then ChessWhite else ChessBlack) | i <- [0..7]] | j <- [0..7]]
