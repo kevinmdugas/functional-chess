6d2edd772d61c842de15e8ab4ee92c0003cec4cb
Adrik Gurganus <adrik@pdx.edu>
2023-03-09 18:08:49 -0800

    parser test cases and extra input validation

--- src/LANParser.hs
+++ src/LANParser.hs
@@ -1,9 +1,8 @@
 module LANParser(parseMove) where
 
-import Piece
+import Board
 import Data.Char (ord)
 
-type Pos = (Int, Int)
 type ChessMove = (Maybe Piece, Pos, Pos)
 
 parseMove :: String -> ChessColor -> (Maybe ChessMove, Maybe ChessMove)
@@ -13,15 +12,19 @@ parseMove lanStr clr =
     "0-0"   -> castleKS clr
     "O-O-O" -> castleQS clr
     "0-0-0" -> castleQS clr
-    [file1, rank1, _, file2, rank2] | validSquare file1 rank1 && 
-                                      validSquare file2 rank2 -> do
+    [file1, rank1, capture, file2, rank2]
+      | validSquare file1 rank1 && 
+        validCapture capture &&
+        validSquare file2 rank2 -> do
       let startPos  = (parseRank rank1, parseFile file1)
           endPos    = (parseRank rank2, parseFile file2)
           pieceType = P
       (return (Just Piece { color = clr, ptype = pieceType }, startPos, endPos), Nothing)
-    [piece, file1, rank1, _, file2, rank2] | validPiece piece && 
-                                             validSquare file1 rank1 && 
-                                             validSquare file2 rank2 -> do
+    [piece, file1, rank1, capture, file2, rank2]
+      | validPiece piece && 
+        validSquare file1 rank1 && 
+        validCapture capture &&
+        validSquare file2 rank2 -> do
       let startPos  = (parseRank rank1, parseFile file1)
           endPos    = (parseRank rank2, parseFile file2)
           pieceTypeM = parsePiece piece
@@ -36,11 +39,14 @@ stripCheck (x:xs)
   | x == '+' || x == '#' = []
   | otherwise = x : stripCheck xs
 
+validPiece :: Char -> Bool
+validPiece p = p `elem` "KQRBNP"
+
 validSquare :: Char -> Char -> Bool
 validSquare file rank = file `elem` "abcdefgh" && rank `elem` "12345678"
 
-validPiece :: Char -> Bool
-validPiece p = p `elem` "KQRBNP"
+validCapture :: Char -> Bool
+validCapture c = c `elem` "-x"
 
 parseFile :: Char -> Int
 parseFile file = ord file - ord 'a'
