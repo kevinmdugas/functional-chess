002ba14b274ac39347ec6b34504e76c776249827
dev <kdugas@pdx.edu>
2023-03-19 14:19:37 -0700

    Abstracted piece validations to standard wrapper

--- src/Validate.hs
+++ src/Validate.hs
@@ -4,28 +4,31 @@ import Board
 import State
 
 validate :: (Maybe ChessMove, Maybe ChessMove) -> ChessColor -> GameState -> Bool
-validate (Just (expected, start, end), Nothing) player state = do
-  let actual = getPiece state start
-  validStartPos expected actual && ( case ptype expected of
-      P -> True
-      _ -> False )
+validate (Just standardMove, Nothing) player state = 
+  validateStandard standardMove player state
 validate (Just kingMove, Just rookMove) player state =
   validateCastle (kingMove, rookMove) player state
 validate (_, _) _ _ = False
 
+validateStandard :: ChessMove -> ChessColor -> GameState -> Bool
+validateStandard (expectedStart, start, end) player state = do
+  let actualStart = getPiece state start
+  let actualEnd = getPiece state end
+  validStartPos expectedStart actualStart &&
+    validEndPos player actualEnd && 
+    ( case ptype expectedStart of
+        P -> True
+        _ -> False )
 -- validatePawn
 
 validateCastle :: (ChessMove, ChessMove) -> ChessColor -> GameState -> Bool
-validateCastle ((k, ks, ke), (r, rs, re)) player state =
-  validFirstMove k (getPiece state ks) &&
-  validFirstMove r (getPiece state rs) &&
-  validEndPos player (getPiece state ke) &&
-  validEndPos player (getPiece state re)
-
-validFirstMove :: Piece -> Maybe Piece -> Bool
-validFirstMove _ Nothing = False
-validFirstMove expected (Just actual) = 
-  expected == actual && not (moved actual)
+validateCastle ((k, ks, ke), (r, rs, re)) player state = do
+  let actualK = getPiece state ks
+  let actualR = getPiece state rs
+  validStartPos k actualK && firstMove actualK &&
+    validStartPos r actualR && firstMove actualR &&
+    validEndPos player (getPiece state ke) &&
+    validEndPos player (getPiece state re)
 
 validEndPos :: ChessColor -> Maybe Piece -> Bool
 validEndPos _ Nothing = True
@@ -34,3 +37,7 @@ validEndPos c (Just p) = color p == oppColor c
 validStartPos :: Piece -> Maybe Piece -> Bool
 validStartPos _ Nothing = False
 validStartPos expected (Just actual) = expected == actual
+
+firstMove :: Maybe Piece -> Bool
+firstMove Nothing = False
+firstMove (Just p) = not (moved p)
