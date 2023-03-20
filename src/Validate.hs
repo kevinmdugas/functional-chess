d1e11fd645ac8ae14bad4cb8a42ed0d228838242
dev <kdugas@pdx.edu>
2023-03-19 18:33:42 -0700

    Comment out validation file

--- src/Validate.hs
+++ src/Validate.hs
@@ -3,6 +3,11 @@ module Validate (validate) where
 import Board
 import State
 
+import Data.Maybe
+
+data Direction = H | V | D deriving (Eq)
+type Vector = [Maybe Piece]
+
 validate :: (Maybe ChessMove, Maybe ChessMove) -> ChessColor -> GameState -> Bool
 validate (Just standardMove, Nothing) player state = 
   validateStandard standardMove player state
@@ -19,7 +24,6 @@ validateStandard (expectedStart, start, end) player state = do
     ( case ptype expectedStart of
         P -> True
         _ -> False )
--- validatePawn
 
 validateCastle :: (ChessMove, ChessMove) -> ChessColor -> GameState -> Bool
 validateCastle ((k, ks, ke), (r, rs, re)) player state = do
@@ -29,6 +33,29 @@ validateCastle ((k, ks, ke), (r, rs, re)) player state = do
     validStartPos r actualR && firstMove actualR &&
     validEndPos player (getPiece state ke) &&
     validEndPos player (getPiece state re)
+
+validatePawn :: ChessColor -> Pos -> Pos -> GameState -> Bool
+validatePawn player (sr, sc) end state = case end of
+  (sr+1, sc) -> isNothing (getPiece state end)
+  (sr+2, sc) -> firstMove startPiece
+  (sr+1, sc+1) ->
+  (sr+1. sc-1) ->
+  (_, _) -> False
+
+createVector :: Direction -> Pos -> Pos -> GameState -> Maybe Vector
+createVector dir (sr, sc) (er, ec) state = case dir of
+  V -> if sc /= ec then Nothing else do
+        let (s, e) = getVerticalBounds sr er
+        Just [getPiece state (i,sc) | i <- [s..e]]
+
+getVerticalBounds :: Int -> Int -> Pos
+getVerticalBounds sr er | sr+1 > er = (er, sr+1)
+                        | otherwise = (sr+1, er)
+
+isClearPath :: Maybe Vector -> Bool
+isClearPath Nothing = False
+isClearPath (Just []) = True
+isClearPath (Just vs) = foldr ((&&) . isNothing) True vs
 
 validEndPos :: ChessColor -> Maybe Piece -> Bool
 validEndPos _ Nothing = True
